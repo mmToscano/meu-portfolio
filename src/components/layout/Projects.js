efb2b1b9576fa6992c4db4da07bb0f5683139562
Matheus Toscano <m.moraes11@outlook.com>
2024-03-18 07:38:55 -0300

    selection_area melhorada

--- src/components/layout/Projects.js
+++ src/components/layout/Projects.js
@@ -1,28 +1,51 @@
 import ProjectImgContainer from "./ProjectImgContainer";
 import styles from "./Projects.module.css";
 
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import projects from "../../fake_database/ProjectsList";
 
 function Projects() {
 
+    const [showFront, setShowFront] = useState(true);
+    const [showBack, setShowBack] = useState(false);
+    const [chosenElement, setChosenElement] = useState(null);
+
+   const mudarSelecao = (event) => {
+    var clickedElement = event.target;
+
+    if(!(clickedElement == chosenElement)){
+        clickedElement.classList.add(styles.chosen);
+    chosenElement.classList.remove(styles.chosen);
+
+    setChosenElement(clickedElement);
+    
+    setShowBack(!showBack);
+    setShowFront(!showFront);
+    }
+
+   }
+
     useEffect(() => {
-        console.log(projects)
+        setChosenElement(document.getElementById("chosen")); //coloca o front como selected
     }, [])
 
     return (
-        <div className={styles.principal}>
+        <div id="projects" className={styles.principal}>
             <h1>Projetos</h1>
             <div className={styles.infoArea}>
                 <div className={styles.selectionArea}> 
                     <div className={styles.buttonArea}>
-                        <h2>Back-end</h2>
-                        <h2 className={styles.chosen}>Front-end</h2>
+                        <h2 onClick={mudarSelecao}>Back-end</h2>
+                        <h2 onClick={mudarSelecao} className={styles.chosen} id="chosen">Front-end</h2>
                     </div>
                     <div className={styles.imgArea}>
-                        {projects.length > 0 &&
-                        projects.map((proj) => (
+                        {projects.length > 0 && showFront &&
+                        projects.filter((project => project.type == "Front-end")).map((proj) => (
+                            <ProjectImgContainer src={proj.src}/>
+                        ))}
+                        {projects.length > 0 && showBack &&
+                        projects.filter((project => project.type == "Back-end")).map((proj) => (
                             <ProjectImgContainer src={proj.src}/>
                         ))}
                     </div>
--- src/fake_database/ProjectsList.js
+++ src/fake_database/ProjectsList.js
@@ -1,5 +1,5 @@
-var ProjectsList = [{id: 0, type: "Front-end", name: "Costs", description: "projeto costs", src: "/img/imagemIlustrativa.png"},
-{id: 1, type: "Front-end", name: "amaz", description: "projeto amaz", src: "/img/imagemIlustrativa.png"},
-{id: 2, type: "Back-end", name: "meu-portfolio", description: "projeto portfolio", src: "/img/imagemIlustrativa.png"}];
+var ProjectsList = [{id: 0, type: "Front-end", name: "Costs", description: "projeto costs", src: "/img/imagemIlustrativa.png", imgs: ["/img/demo1", "/img/demo2", "/img/demo3"]},
+{id: 1, type: "Front-end", name: "amaz", description: "projeto amaz", src: "/img/imagemIlustrativa.png", imgs: ["/img/demo1", "/img/demo2", "/img/demo3"]},
+{id: 2, type: "Back-end", name: "meu-portfolio", description: "projeto portfolio", src: "/img/imagemIlustrativa.png", imgs: ["/img/demo1", "/img/demo2", "/img/demo3"]}];
 
 export default ProjectsList;
